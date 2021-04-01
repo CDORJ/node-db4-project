a78b2cfdbfc821a60c615d6f5d9954ffa65e2994
ChadDiaz <chadcdiaz@gmail.com>
2021-04-01 16:05:44 -0400

    adjusted schema and migration file accordingly

--- data/migrations/20210401184706_create-tables.js
+++ data/migrations/20210401184706_create-tables.js
@@ -2,17 +2,17 @@ exports.up = function (knex) {
   return knex.schema
     .createTable("recipes", (tbl) => {
       tbl.increments();
-      tbl.string("recipe_name").notNullable().unique();
+      tbl.string("recipe_name", 128).notNullable();
       tbl.timestamp(false, true);
     })
     .createTable("ingredients", (tbl) => {
       tbl.increments();
-      tbl.string("ingredient_name", 128).notNullable().unique();
+      tbl.string("ingredient_name", 250);
     })
     .createTable("steps", (tbl) => {
       tbl.increments();
-      tbl.integer("step_number").notNullable();
-      tbl.string("instructions", 250).notNullable();
+      tbl.integer("step_number");
+      tbl.string("step", 250);
       tbl
         .integer("recipe_id")
         .notNullable()
@@ -21,13 +21,13 @@ exports.up = function (knex) {
         .onDelete("CASCADE")
         .onUpdate("CASCADE");
     })
-    .createTable("step_ingredients", (tbl) => {
+    .createTable("recipes_ingredients", (tbl) => {
       tbl.increments();
       tbl
-        .integer("step_id")
+        .integer("recipe_id")
         .notNullable()
         .unsigned()
-        .references("steps.id")
+        .references("recipes.id")
         .onDelete("CASCADE")
         .onUpdate("CASCADE");
       tbl
@@ -37,12 +37,13 @@ exports.up = function (knex) {
         .references("ingredients.id")
         .onDelete("CASCADE")
         .onUpdate("CASCADE");
+      tbl.float("quantity").unsigned();
     });
 };
 
 exports.down = function (knex) {
   return knex.scheme
-    .dropTableIfExists("step_ingredients")
+    .dropTableIfExists("recipes_ingredients")
     .dropTableIfExists("steps")
     .dropTableIfExists("ingredients")
     .dropTableIfExists("recipes");
